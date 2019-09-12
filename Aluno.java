9fea412eb8a1a55bd0a8e906a22270acaf1f7718
Juhrer <54405118+Juhrer@users.noreply.github.com>
2019-09-12 19:49:06 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,12 +1,14 @@
 package lab4;
 
 public class Aluno {
-	private String nome, curso;
-	public Aluno(String nome, String curso) {
+	private String nome, curso, matricula;
+	public Aluno(String nome, String curso, String matricula) {
 		this.nome = nome;
 		this.curso = curso;
+		this.matricula = matricula;
 	}
+	@Override
 	public String toString() {
-		return " - " + nome + " - " + curso;
+		return matricula + " - " + nome + " - " + curso;
 	}
 }

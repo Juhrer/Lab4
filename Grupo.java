8f82280c1b35cd0d2426302262764a1566f9ee02
Juhrer <54405118+Juhrer@users.noreply.github.com>
2019-09-12 19:49:55 -0300

    Update Grupo.java

--- Grupo.java
+++ Grupo.java
@@ -1,34 +1,22 @@
 package lab4;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Grupo {
-	private ArrayList alunos = new ArrayList();
-	private String nome;
+	public String nome;
+	private HashSet<Aluno> alunos = new HashSet<>();
 	public Grupo(String nome) {
 		this.nome = nome;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		return result;
+	public void alocaAluno(Aluno aluno) {
+		alunos.add(aluno);
 	}
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Grupo other = (Grupo) obj;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		return true;
+	public String toString() {
+		String listaAlunos = "Alunos do Grupo " + nome + ":\n";
+		for(Aluno aluno : alunos) {
+			listaAlunos += "* " + aluno.toString() + "\n";
+		}
+		return  listaAlunos;
 	}
 }

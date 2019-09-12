943a5513e9b1902054a9d9c5c7d7752de7d7b964
Juhrer <54405118+Juhrer@users.noreply.github.com>
2019-09-12 19:49:46 -0300

    Update ControleAlunos.java

--- ControleAlunos.java
+++ ControleAlunos.java
@@ -4,33 +4,63 @@ import java.util.*;
 
 public class ControleAlunos {
 	private HashMap<String, Aluno> alunos = new HashMap<>();
-	private HashSet<Grupo> grupos = new HashSet<>();
+	private HashMap<String, Grupo> grupos = new HashMap<>();
+	private ArrayList<Aluno> alunosResponderam = new ArrayList<>();
 	public String cadastraAluno(String matricula, String nomeAluno, String curso) {
 		if(alunos.containsKey(matricula)) {
 			return "MATRÍCULA JÁ CADASTRADA";
 		} else {
-			alunos.put(matricula, new Aluno(nomeAluno, curso));
+			alunos.put(matricula, new Aluno(nomeAluno, curso, matricula));
 			return "CADASTRO REALIZADO!";
 		}
 	}
 	public String consultaAluno(String matricula) {
 		if(!alunos.containsKey(matricula)) {
-			return "Aluno não cadastrado";
+			return "Aluno não cadastrado.";
 		} else {
-			return "Aluno: " + matricula + alunos.get(matricula).toString();
+			return "Aluno: " + alunos.get(matricula).toString();
 		}
 	}
 	public String cadastrarGrupo(String nomeGrupo) {
-		if(grupos.contains(new Grupo(nomeGrupo.toUpperCase()))) {
+		if(grupos.containsKey(nomeGrupo.toUpperCase())) {
 			return "GRUPO JÁ CADASTRADO!";
 		} else {
-			grupos.add(new Grupo(nomeGrupo.toUpperCase()));
+			grupos.put(nomeGrupo.toUpperCase(), new Grupo(nomeGrupo));
 			return "CADASTRO REALIZADO!";
 		}
 	}
-	public String alocaAluno(String matricula, String nomeGrupo) {
-		if(!alunos.containsKey(matricula) && !grupos.contains(nomeGrupo.toUpperCase())) {
-			return "
+	public String alocaAluno(String matricula, String nomeGrupoMaiusculo) {
+		if(!alunos.containsKey(matricula) && !grupos.containsKey(nomeGrupoMaiusculo)) {
+			return "Aluno e grupo não cadastrados.";
+		} else if(!alunos.containsKey(matricula)) {
+			return "Aluno não cadastrado.";
+		} else if(!grupos.containsKey(nomeGrupoMaiusculo)) {
+			return "Grupo não cadastrado.";
+		} else {
+			grupos.get(nomeGrupoMaiusculo).alocaAluno(alunos.get(matricula));
+			return "ALUNO ALOCADO!";
 		}
+	}
+	public String imprimiGrupo(String nomeGrupoMaiusculo) {
+		if(!grupos.containsKey(nomeGrupoMaiusculo)) {
+			return "Grupo não cadastrado.";
+		} else {
+			return grupos.get(nomeGrupoMaiusculo).toString();
+		}
+	}
+	public String cadastrarAlunosResponderam(String matricula) {
+		if(!alunos.containsKey(matricula)) {
+			return "Aluno não cadastrado.";
+		} else {
+			alunosResponderam.add(alunos.get(matricula));
+			return "ALUNO REGISTRADO!";
+		}
+	}
+	public String imprimiAlunosResponderam() {
+		String listaAlunosResponderam = "Alunos:\n";
+		for(int i = 0; i < alunosResponderam.size(); i++) {
+			listaAlunosResponderam += i + 1 + ". " + alunosResponderam.get(i).toString() + "\n";
+		}
+		return listaAlunosResponderam;
 	}
 }

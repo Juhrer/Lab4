71e88d94aec052c704a78dfdf82db02eb377722a
Juhrer <54405118+Juhrer@users.noreply.github.com>
2019-09-19 13:28:28 -0300

    Update ControleAlunosTeste.java

--- ControleAlunosTeste.java
+++ ControleAlunosTeste.java
@@ -8,60 +8,61 @@ class ControleAlunosTeste {
 	@BeforeEach
 	void criaControleAlunos() {
 		controle = new ControleAlunos();
-		controle.cadastraAluno("11111111", "Lucas", "ComputaÁ„o");
-		controle.cadastraAluno("1234567", "Aeiou", "ElÈtrica");
+		controle.cadastraAluno("11111111", "Lucas", "Computa√ß√£o");
+		controle.cadastraAluno("1234567", "Aeiou", "El√©trica");
 		controle.cadastraAluno("555555555","Matheus", "Direito");
 		controle.cadastrarGrupo("GrUpo1");
 		controle.cadastrarGrupo("Grupo2");
 		controle.alocaAluno("11111111", "GRUPO1");
 		controle.alocaAluno("1234567", "GRUPO1");
-		controle.cadastrarAlunosResponderam("11111111");
-		controle.cadastrarAlunosResponderam("1234567");
+		controle.registraAlunoRespondeu("11111111");
+		controle.registraAlunoRespondeu("1234567");
+		controle.registraAlunoRespondeu("11111111");
 	}
 
 	@Test
 	void testCadastraAluno() {
-		assertEquals("CADASTRO REALIZADO!", controle.cadastraAluno("9999999", "Aaaaa", "Letras"));
-		assertEquals("MATRÕCULA J¡ CADASTRADA", controle.cadastraAluno("11111111", "Aeiou", "ElÈtrica"));
+		assertEquals("CADASTRO REALIZADO!\n", controle.cadastraAluno("9999999", "Aaaaa", "Letras"));
+		assertEquals("MATR√çCULA J√Å CADASTRADA\n", controle.cadastraAluno("11111111", "Aeiou", "El√©trica"));
 	}
 
 	@Test
-	void testConsultaAluno() {
-		assertEquals("Aluno n„o cadastrado.", controle.consultaAluno("7654321"));
-		assertEquals("Aluno: 11111111 - Lucas - ComputaÁ„o", controle.consultaAluno("11111111"));
-		assertEquals("Aluno: 1234567 - Aeiou - ElÈtrica", controle.consultaAluno("1234567"));
+	void testExibiAluno() {
+		assertEquals("\nAluno n√£o cadastrado.\n", controle.exibiAluno("7654321"));
+		assertEquals("\nAluno: 11111111 - Lucas - Computa√ß√£o\n", controle.exibiAluno("11111111"));
+		assertEquals("\nAluno: 1234567 - Aeiou - El√©trica\n", controle.exibiAluno("1234567"));
 	}
 
 	@Test
 	void testCadastrarGrupo() {
-		assertEquals("GRUPO J¡ CADASTRADO!", controle.cadastrarGrupo("grupo1"));
-		assertEquals("CADASTRO REALIZADO!", controle.cadastrarGrupo("Grupo3"));
+		assertEquals("GRUPO J√Å CADASTRADO!\n", controle.cadastrarGrupo("grupo1"));
+		assertEquals("CADASTRO REALIZADO!\n", controle.cadastrarGrupo("Grupo3"));
 	}
 
 	@Test
 	void testAlocaAluno() {
-		assertEquals("Aluno e grupo n„o cadastrados.", controle.alocaAluno("00000000", "GRUPO4"));
-		assertEquals("Aluno n„o cadastrado.", controle.alocaAluno("00000000","GRUPO1"));
-		assertEquals("Grupo n„o cadastrado.", controle.alocaAluno("1234567", "GRUPO4"));
-		assertEquals("ALUNO ALOCADO!", controle.alocaAluno("1234567", "GRUPO1"));
-		assertEquals("ALUNO ALOCADO!", controle.alocaAluno("555555555", "GRUPO1"));
+		assertEquals("Aluno e grupo n√£o cadastrados.\n", controle.alocaAluno("00000000", "GRUPO4"));
+		assertEquals("Aluno n√£o cadastrado.\n", controle.alocaAluno("00000000","GRUPO1"));
+		assertEquals("Grupo n√£o cadastrado.\n", controle.alocaAluno("1234567", "GRUPO4"));
+		assertEquals("ALUNO ALOCADO!\n", controle.alocaAluno("1234567", "GRUPO1"));
+		assertEquals("ALUNO ALOCADO!\n", controle.alocaAluno("555555555", "GRUPO1"));
 	}
 
 	@Test
 	void testImprimiGrupo() {
-		assertEquals("Grupo n„o cadastrado.", controle.imprimiGrupo("GRUPO4"));
-		assertEquals("Alunos do Grupo Grupo2:\n", controle.imprimiGrupo("GRUPO2"));
-		assertEquals("Alunos do Grupo GrUpo1:\n* 1234567 - Aeiou - ElÈtrica\n* 11111111 - Lucas - ComputaÁ„o\n", controle.imprimiGrupo("GRUPO1"));
+		assertEquals("Grupo n√£o cadastrado.\n", controle.imprimiGrupo("GRUPO4"));
+		assertEquals("\nAlunos do Grupo Grupo2:\n", controle.imprimiGrupo("GRUPO2"));
+		assertEquals("\nAlunos do Grupo GrUpo1:\n* 1234567 - Aeiou - El√©trica\n* 11111111 - Lucas - Computa√ß√£o\n", controle.imprimiGrupo("GRUPO1"));
 	}
 
 	@Test
-	void testCadastrarAlunosResponderam() {
-		assertEquals("Aluno n„o cadastrado.", controle.cadastrarAlunosResponderam("2222222"));
-		assertEquals("ALUNO REGISTRADO!", controle.cadastrarAlunosResponderam("11111111"));
+	void testRegistraAlunoRespondeu() {
+		assertEquals("Aluno n√£o cadastrado.\n", controle.registraAlunoRespondeu("2222222"));
+		assertEquals("ALUNO REGISTRADO!\n", controle.registraAlunoRespondeu("11111111"));
 	}
 
 	@Test
 	void testImprimiAlunosResponderam() {
-		assertEquals("Alunos:\n1. 11111111 - Lucas - ComputaÁ„o\n2. 1234567 - Aeiou - ElÈtrica\n", controle.imprimiAlunosResponderam());
+		assertEquals("Alunos:\n1. 11111111 - Lucas - Computa√ß√£o\n2. 1234567 - Aeiou - El√©trica\n3. 11111111 - Lucas - Computa√ß√£o\n", controle.imprimiAlunosResponderam());
 	}
 }
